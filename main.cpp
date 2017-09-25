4e912d6cc33b409cc3b4ddbcea21693609215ba7
jakublada <kuba.lada.1996@wp.pl>
2017-09-25 15:28:07 +0200

    Podsumowanie

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+     int aktualny_rok = 2017;
+    int rok_urodzenia=1996;
+    int wiek=aktualny_rok - rok_urodzenia;
+string nazwisko="Lada";
+    string imie ="Kuba";
+    cout <<"Nazywam sie "<<imie<<""<<nazwisko<<" Mam "<<wiek<<" lat"<<endl;
+    return 0;
+}
